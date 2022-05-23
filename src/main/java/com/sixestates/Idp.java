a076fd813a72d78679d4ccf9f63848aa292fafb6
“anyang” <anyang@6estates.com>
2022-05-23 17:17:02 +0800

    release v1

--- src/main/java/com/sixestates/Idp.java
+++ src/main/java/com/sixestates/Idp.java
@@ -11,7 +11,7 @@ import com.sixestates.http.IdpRestClient;
  */
 public class Idp {
 
-    public static final String VERSION = "8.29.0";
+    public static final String VERSION = "1.0";
     public static final String JAVA_VERSION = System.getProperty("java.version");
 
     private static String customer = System.getenv("IDP_CUSTOMER");
